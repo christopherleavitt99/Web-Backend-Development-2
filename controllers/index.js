601ccb88a257822a3122514b3b2d5fa84993c845
christopherleavitt99 <cml95@byui.edu>
2023-01-11 12:43:34 -0700

    First commit first assignment

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const awesomeFunction = (req, res, next) => {
+    res.json('Denise Leavitt');
+};
+
+const returnAnotherPerson = (req, res, next) => {
+    res.json('Super awesome person');
+};
+
+module.exports = { awesomeFunction, returnAnotherPerson };
