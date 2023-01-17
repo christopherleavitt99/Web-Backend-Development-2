34642cea8b85c3f278ec3b42959a917dc556a588
christopherleavitt99 <cml95@byui.edu>
2023-01-17 10:48:44 -0700

    added eslint and prettier

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,7 +1,7 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res, next) => {
+const getAll = async (req, res) => {
   const result = await mongodb.getDb().db().collection('Contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
@@ -9,13 +9,9 @@ const getAll = async (req, res, next) => {
   });
 };
 
-const getSingle = async (req, res, next) => {
+const getSingle = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('Contacts')
-    .find({ _id: userId });
+  const result = await mongodb.getDb().db().collection('Contacts').find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
