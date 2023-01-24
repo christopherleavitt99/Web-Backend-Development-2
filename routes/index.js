dde754047f66b5cfc43f9c6a3b4455c3d6547a40
christopherleavitt99 <cml95@byui.edu>
2023-01-24 08:36:00 -0700

    Lesson 4

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,12 @@
 const express = require('express');
 const router = express.Router();
 router.use('/contacts', require('./contacts'));
+const swaggerAutogen = require('../swagger-autogen')();
+
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
