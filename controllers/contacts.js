e49ecff986811dcc68f1bfd9025e5fc414674806
christopherleavitt99 <cml95@byui.edu>
2023-01-24 16:33:13 -0700

    added try and catch to all the functions

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,22 +2,31 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+  try {
   const result = await mongodb.getDb().db().collection('Contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
+} catch (err) {
+  res.status(500).json({ message: err.message });
+}
 };
 
 const getSingle = async (req, res) => {
+  try {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db().collection('Contacts').find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   });
+} catch (err) {
+  res.status(500).json({ message: err.message});
+}
 };
 const createContact = async (req, res) => {
+  try {
   const contact = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -31,9 +40,13 @@ const createContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while creating the contact.');
   }
+} catch (err) {
+  res.status(500).json({ message: err.message});
+}
 };
 
 const updateContact = async (req, res) => {
+  try {
   const userId = new ObjectId(req.params.id);
 
   const contact = {
@@ -54,9 +67,13 @@ const updateContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while updating the contact.');
   }
+}catch (err) {
+  res.status(500).json({ message: err.message});
+}
 };
 
 const deleteContact = async (req, res) => {
+  try {
   const userId = new ObjectId(req.params.id);
   const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
   console.log(response);
@@ -65,6 +82,9 @@ const deleteContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
   }
+} catch (err) {
+  res.status(500).json({ message: err.message});
+}
 };
 
 module.exports = {
