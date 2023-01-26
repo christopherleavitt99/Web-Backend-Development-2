41a5ce95616935b5910d99a4947c322a87a09f18
christopherleavitt99 <cml95@byui.edu>
2023-01-26 09:00:04 -0700

    Add swaggerUi and document

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
+routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
