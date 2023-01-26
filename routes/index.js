a7744319a193c30b2e0adfc03cd7e11735e8b099
christopherleavitt99 <cml95@byui.edu>
2023-01-26 09:04:11 -0700

    routes to router

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
