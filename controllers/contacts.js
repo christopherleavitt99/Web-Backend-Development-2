c3befaee61dc542fbd316cf7aa5b0676ba6a509c
christopherleavitt99 <cml95@byui.edu>
2023-01-26 13:36:11 -0700

    lowercase c in "contacts"

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,7 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
   try {
-    const result = await mongodb.getDb().db().collection('Contacts').find();
+    const result = await mongodb.getDb().db().collection('contacts').find();
     result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists);
@@ -16,7 +16,7 @@ const getAll = async (req, res) => {
 const getSingle = async (req, res) => {
   try {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('Contacts').find({ _id: userId });
+    const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
     result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists[0]);
