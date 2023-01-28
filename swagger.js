9d2d18dbe4815da1a6e1e8c77d84054c7269d86b
christopherleavitt99 <cml95@byui.edu>
2023-01-28 12:52:54 -0700

    removed https://

--- swagger.js
+++ swagger.js
@@ -7,7 +7,7 @@ const doc = {
   },
   // host: 'localhost:3000',
   // schemes: ['http']
-    host: 'https://chrisleavitt4.onrender.com',
+    host: 'chrisleavitt4.onrender.com',
     schemes: ['https']
 };
 
